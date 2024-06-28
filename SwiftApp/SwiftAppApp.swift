3be8204bbe907ff2e8fc491f9f8c8f5a07bedce1
kodi <28621241+ko-di@users.noreply.github.com>
2024-06-28 16:39:40 +0100

    Initial Commit

---
+++ SwiftApp/SwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppApp.swift
+//  SwiftApp
+//
+//  Created by kodi on 28/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
